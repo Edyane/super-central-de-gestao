aa7b358790839b3e267fcc33b411b0eca8fb7f0b
Edyane <edyanealves@gmail.com>
2025-11-20 19:43:47 -0300

    feat: adicionando componente card e seus derivados

---
+++ super-central-gestao/src/components/ui/Card.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { cn } from '@/lib/utils'
+
+defineProps({
+  class: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div :class="cn('rounded-lg bg-white text-card-foreground', $props.class)">
+    <slot />
+  </div>
+</template>
+
+
